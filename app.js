e4cfea46544a0fa4bd7ad8356b337f5ecdc50810
Jrperez79 <joelrperez@gmail.com>
2020-07-08 20:21:50 -0700

    changed random-throw.js

--- app.js
+++ app.js
@@ -3,32 +3,19 @@ import { getRandomThrow, checkResults } from './random-throw.js';
 
 //DOM elements
 const myButton = document.getElementById('guess-button');
+const cpuPick = document.getElementById('cpu-pick');
 const current = document.getElementById('current');
-const wins = document.getElementById('wins');
-const losses = document.getElementById('losses');
-const draws = document.getElementById('draws');
-const total = document.getElementById('total');
+const winCount = document.getElementById('wins');
+const losscount = document.getElementById('losses');
+const drawCount = document.getElementById('draws');
+const totalCount = document.getElementById('total');
 
 // initialize state
 // Total Wins Losses Draws
 let userWins = 0;
 let userLoss = 0;
 let userDraws = 0;
-// Total times you played 'hit the guess button'
-let totalGuesses = 0;
+let totalGames = 0;
 
 // set event listeners to update state and DOM
 
-function useResults(result) {
-    if (result === 'You Won!') {
-        userWins++;
-    } else if (result === 'You Lost') {
-        userLoss++;
-    } else {
-        userDraws++;
-    }
-    totalGuesses++;
-    current.textContent = `You picked ${result}!`;
-    
-
-}
--- random-throw.js
+++ random-throw.js
@@ -1,20 +1,26 @@
 export function getRandomThrow() {
     const randomNumber = Math.floor(Math.random() * 3);
-    const throwOptions = ['Rock', 'Paper', 'Scissors'];
-
-    return throwOptions[randomNumber];
+    if (randomNumber === 1) {
+        return 'Rock';
+    } else if (randomNumber === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
 }
 
 export function checkResults(cpuThrow, playerThrow) {
-    if (cpuThrow === playerThrow) {
-        return 'Tie';
+    if (cpuThrow === 'Rock' && playerThrow === 'Scissors') {
+        return 'You Won!';
     } else if (cpuThrow === 'Rock' && playerThrow === 'Paper') {
-        return 'Winner';
-    } else if (cpuThrow === 'Paper' && playerThrow === 'Scissors') {
-        return 'Winner';
-    } else if (cpuThrow === 'Scissors' && playerThrow === 'Rock') {
-        return 'Winner';
+        return 'You Won!';
+    } else if (cpuThrow === 'Paper' && playerThrow === 'Rock') {
+        return 'You Lose!';
+    } else if (cpuThrow === 'Rock' && playerThrow === 'Scissor') {
+        return 'You Lose!';
+    } else if (cpuThrow === 'Scissors' && playerThrow === 'Paper') {
+        return 'You Lose!';
     } else {
-        return 'Loser';
+        return 'It is a draw!';
     }
 }
