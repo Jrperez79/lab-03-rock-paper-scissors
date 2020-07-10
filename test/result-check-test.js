08f3f98702883351530ca9137e1b1da06c6e1bcf
Jrperez79 <joelrperez@gmail.com>
2020-07-09 18:39:28 -0700

    finished app!

--- test/result-check-test.js
+++ test/result-check-test.js
@@ -3,7 +3,7 @@ import { checkResults } from '../random-throw.js';
 
 const test = QUnit.test;
 
-test('Rock, Paper, Scissors', function(assert) {
+test('Paper vs Scissors = loss, Paper vs Rock = Won, Paper vs Paper = Draw', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const player = 'Paper';
@@ -21,9 +21,9 @@ test('Rock, Paper, Scissors', function(assert) {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const result = checkResults(player, computer);
-    const result2 = checkResults(player2, computer2);
-    const result3 = checkResults(player3, computer3);
+    const result = checkResults(computer, player);
+    const result2 = checkResults(computer2, player2);
+    const result3 = checkResults(computer3, player3);
     //Assert
     // Make assertions about what is expected valid result
     assert.equal(result, expected);
